fa722379689d8d4ffa6e147149c47f5eb17f0261
Tom <tkrywit@gmail.com>
2014-12-07 13:42:53 -0700

    Added listener to Recyclerview

--- app/src/main/java/com/priori/tkrywit/priori/RecyclerViewAdapter.java
+++ app/src/main/java/com/priori/tkrywit/priori/RecyclerViewAdapter.java
@@ -1,17 +1,19 @@
 package com.priori.tkrywit.priori;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 /**
  * Created by Thomas on 11/28/2014.
  */
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ListItemViewHolder> {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RecyclerItemViewHolder> {
 
     public final ArrayList<Task> tasks;
 
@@ -22,22 +24,27 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     // Create new views (invoked by the layout manager)
     @Override
-    public ListItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view based on task list row
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.task_list_row, parent, false);
 
-        return new ListItemViewHolder(v);
+        return new RecyclerItemViewHolder(v, new RecyclerItemViewHolder.ViewHolderClick() {
+            public void onItemClick(int position) {
+                String s = String.valueOf(position);
+                Log.d("GUBS", "Pressed!" + s);
+            }
+
+        });
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ListItemViewHolder holder, int position) {
+    public void onBindViewHolder(RecyclerItemViewHolder holder, int position) {
 
         //bind views to data
         holder.taskTitleTextView.setText(tasks.get(position).getTitle());
         holder.taskDescTextView.setText(tasks.get(position).getDesc());
-
     }
 
     // Return the size of your data set (invoked by the layout manager)
@@ -46,18 +53,35 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return tasks.size();
     }
 
-    public final static class ListItemViewHolder extends RecyclerView.ViewHolder {
+    public final static class RecyclerItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         //views
         public TextView taskTitleTextView;
         public TextView taskDescTextView;
 
+        //viewholder click
+        public ViewHolderClick vc;
+
         //bind views
-        public ListItemViewHolder(View itemView) {
+        public RecyclerItemViewHolder(View itemView, ViewHolderClick listener) {
             super(itemView);
             taskTitleTextView = (TextView) itemView.findViewById(R.id.taskTitle);
             taskDescTextView = (TextView) itemView.findViewById(R.id.taskDesc);
+
+            //needed?
+            vc = listener;
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            vc.onItemClick(getPosition());
+        }
+
+        public static interface ViewHolderClick {
+            public void onItemClick(int position);
+        }
+
     }
 
 
